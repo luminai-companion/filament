6b35edfd06a37f26e01f6dcfcf7a7664225c2f38
iver0 <jermacoff@gmail.com>
2023-02-23 10:39:47 +0700

    Perform minor refactor and add license

--- app/api.py
+++ app/api.py
@@ -1,16 +1,10 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
 from collections import defaultdict
-import os
 
-# from dotenv import load_dotenv, find_dotenv
 from fastapi import Body, FastAPI, Request
 from starlette.responses import RedirectResponse, JSONResponse
 import httpx
 import spacy
 import srsly
-import uvicorn
 
 from app.models import (
     ENT_PROP_MAP,
@@ -36,64 +30,51 @@ kobold_url = "http://127.0.0.1:5000/api"
 
 
 @app.get("/", include_in_schema=False)
-def docs_redirect():
-    return RedirectResponse(f"/docs")
+def docs_redirect() -> RedirectResponse:
+    return RedirectResponse("/docs")
 
 
 @app.get("/api/{path:path}")
-def handle_get(request: Request, path: str):
+def handle_get(request: Request, path: str) -> JSONResponse:
     url = f"{kobold_url}/{path}"
     r = httpx.get(url)
     return JSONResponse(status_code=r.status_code, content=r.text)
 
 
 @app.post("/entities", response_model=RecordsResponse, tags=["NER"])
-async def extract_entities(body: RecordsRequest = Body(..., example=example_request)):
+async def extract_entities(body: RecordsRequest = Body(..., example=example_request)) -> dict:
     """Extract Named Entities from a batch of Records."""
 
-    res = []
-    documents = []
+    documents = ({"id": val.recordId, "text": val.data.text} for val in body.values)
+    extracted_entities = extractor.extract_entities(documents)
 
-    for val in body.values:
-        documents.append({"id": val.recordId, "text": val.data.text})
-
-    entities_res = extractor.extract_entities(documents)
-
-    res = [
-        {"recordId": er["id"], "data": {"entities": er["entities"]}}
-        for er in entities_res
+    result = [
+        {"recordId": er["id"], "data": {"entities": er["entities"]}} for er in extracted_entities
     ]
 
-    return {"values": res}
+    return {"values": result}
 
 
-@app.post(
-    "/entities_by_type", response_model=RecordsEntitiesByTypeResponse, tags=["NER"]
-)
+@app.post("/entities_by_type", response_model=RecordsEntitiesByTypeResponse, tags=["NER"])
 async def extract_entities_by_type(
     body: RecordsRequest = Body(..., example=example_request)
-):
+) -> dict:
     """Extract Named Entities from a batch of Records separated by entity label.
     This route can be used directly as a Cognitive Skill in Azure Search
     For Documentation on integration with Azure Search, see here:
     https://docs.microsoft.com/en-us/azure/search/cognitive-search-custom-skill-interface
     """
 
-    res = []
-    documents = []
+    documents = ({"id": val.recordId, "text": val.data.text} for val in body.values)
+    extracted_entities = extractor.extract_entities(documents)
 
-    for val in body.values:
-        documents.append({"id": val.recordId, "text": val.data.text})
-
-    entities_res = extractor.extract_entities(documents)
-    res = []
-
-    for er in entities_res:
+    result = []
+    for er in extracted_entities:
         groupby = defaultdict(list)
         for ent in er["entities"]:
             ent_prop = ENT_PROP_MAP[ent["label"]]
             groupby[ent_prop].append(ent["name"])
         record = {"recordId": er["id"], "data": groupby}
-        res.append(record)
+        result.append(record)
 
-    return {"values": res}
+    return {"values": result}
--- app/spacy_extractor.py
+++ app/spacy_extractor.py
@@ -1,66 +1,35 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
-from typing import Dict, List
-import spacy
+from dataclasses import dataclass
+from typing import Dict, Iterable
 from spacy.language import Language
 
 
+@dataclass
 class SpacyExtractor:
-    """class SpacyExtractor encapsulates logic to pipe Records with an id and text body
+    """Encapsulates logic to pipe Records with an id and text body
     through a spacy model and return entities separated by Entity Type
     """
 
-    def __init__(
-        self, nlp: Language, input_id_col: str = "id", input_text_col: str = "text"
-    ):
-        """Initialize the SpacyExtractor pipeline.
+    nlp: Language
+    doc_id_key: str = "id"
+    doc_text_key: str = "text"
 
-        nlp (spacy.language.Language): pre-loaded spacy language model
-        input_text_col (str): property on each document to run the model on
-        input_id_col (str): property on each document to correlate with request
+    def name_to_id(self, text: str) -> str:
+        return text.lower().replace(" ", "-")
 
-        RETURNS (EntityRecognizer): The newly constructed object.
+    def extract_entities(self, records: Iterable[Dict[str, str]]) -> list:
+        """Return a list of dictionaries with extracted entities and their document's ID
+        from an iterable of "document" dictionaries each with an `id` and `text` property
         """
-        self.nlp = nlp
-        self.input_id_col = input_id_col
-        self.input_text_col = input_text_col
+        ids, texts = ((doc[self.doc_id_key], doc[self.doc_text_key]) for doc in records)
 
-    def _name_to_id(self, text: str):
-        """Utility function to do a messy normalization of an entity name
-
-        text (str): text to create "id" from
-        """
-        return "-".join([s.lower() for s in text.split()])
-
-    def extract_entities(self, records: List[Dict[str, str]]):
-        """Apply the pre-trained model to a batch of records
-
-        records (list): The list of "document" dictionaries each with an
-            `id` and `text` property
-
-        RETURNS (list): List of responses containing the id of
-            the correlating document and a list of entities.
-        """
-        ids = (doc[self.input_id_col] for doc in records)
-        texts = (doc[self.input_text_col] for doc in records)
-
-        res = []
-
+        result = []
         for doc_id, spacy_doc in zip(ids, self.nlp.pipe(texts)):
             entities = {}
             for ent in spacy_doc.ents:
-                ent_id = ent.kb_id
-                if not ent_id:
-                    ent_id = ent.ent_id
-                if not ent_id:
-                    ent_id = self._name_to_id(ent.text)
-
+                # Could use explaining why these values can be None and what happens here
+                ent_id = ent.kb_id or ent.ent_id or self.name_to_id(ent.text)
                 if ent_id not in entities:
-                    if ent.text.lower() == ent.text:
-                        ent_name = ent.text.capitalize()
-                    else:
-                        ent_name = ent.text
+                    ent_name = ent.text.capitalize() if ent.text.islower() else ent.text
                     entities[ent_id] = {
                         "name": ent_name,
                         "label": ent.label_,
@@ -70,5 +39,5 @@ class SpacyExtractor:
                     {"start": ent.start_char, "end": ent.end_char, "text": ent.text}
                 )
 
-            res.append({"id": doc_id, "entities": list(entities.values())})
-        return res
+            result.append({"id": doc_id, "entities": list(entities.values())})
+        return result
