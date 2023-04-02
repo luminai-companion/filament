00ac0794596a7e40655f4a0236a548e66dad4a40
Pleasant-Cry <pleasant.cry@gmail.com>
2023-04-01 19:56:44 -0600

    Load .env in the right place.

--- app/config.py
+++ app/config.py
@@ -1,4 +1,8 @@
+from dotenv import load_dotenv
 from pydantic import BaseSettings
+
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import uvicorn
-from dotenv import load_dotenv
-
-load_dotenv()
 
 if __name__ == "__main__":
     uvicorn.run(
